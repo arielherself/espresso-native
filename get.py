dd2941f5078ebf16424ca1b6a8c3edd52bcc35d1
Ariel <107245128+arielherself@users.noreply.github.com>
2022-07-24 14:08:07 +0800

    Update get.py

--- get.py
+++ get.py
@@ -50,6 +50,7 @@ def web_process(**kwargs):
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
         <style type="text/css">
             @font-face {
                 font-family: 'custom';
