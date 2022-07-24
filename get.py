ae170e2d1f6bf0991396184faf4a777aa9a0997f
Ariel <107245128+arielherself@users.noreply.github.com>
2022-07-24 16:54:20 +0800

    Update get.py

--- get.py
+++ get.py
@@ -14,37 +14,27 @@ def get(proxy=False, http_proxy='', https_proxy=''):
     pre = []
     details = []
     titles = []
-    '''pre-content'''
     slice = content[:content.find('<h3')]
     while slice.find('<p>') != -1:
         pre.append(slice[slice.find('<p>')+3:slice.find('</p>')])
         slice = slice[slice.find('</p>')+4:]
-    # print(pre)
     while content.find('<h3') != -1:
         content = content[content.find('<h3')+3:]
         content = content[content.find('>')+1:]
         titles.append(content[:content.find('</h3>')])
         content = content[content.find('</h3>')+5:]
-        # print(content[content.find('<p>')+3:])
         part = content[:content[content.find('<p>')+3:].find('</div>')+content.find('<p>')+3]
-        # print(part)
         ps = []
         while part.find('<p>') != -1:
             ps.append(part[part.find('<p>')+3:part.find('</p')])
             part = part[part.find('</p>')+4:]
             content = content[content.find('</p>')+4:]
         details.append(ps)
-    # while content.find('<p>') != -1:
-
-    # print(titles, details)
-    # print(len(titles), len(details))
     return pre, titles, details
 
 def web_process(**kwargs):
     pre, titles, details = get(**kwargs)
     htmllines = []
-    # htmllines.append('[English](https://github.com/arielherself/espresso/blob/main/README.md)|[中文](https://github-com.translate.goog/arielherself/espresso/blob/main/README.md?_x_tr_sl=en&_x_tr_tl=zh-CN&_x_tr_hl=zh-CN&_x_tr_pto=wapp)\n\n')
-    # htmllines.append('![The Economist](menubar.png)')
     prefix = '''
 <!DOCTYPE html>
 <html>
@@ -96,51 +86,6 @@ def web_process(**kwargs):
         if each == '':
             continue
         processed_detail = each
-        # while processed_detail.find('<strong>') != -1:
-        #     if processed_detail[processed_detail.find('<strong>')-1] == ' ':
-        #         if processed_detail[processed_detail.find('<strong>')+8] == ' ':
-        #             processed_detail = processed_detail[:processed_detail.find('<strong>')]+'**'+processed_detail[processed_detail.find('<strong>')+9:]
-        #         else:
-        #             processed_detail = processed_detail[:processed_detail.find('<strong>')]+'**'+processed_detail[processed_detail.find('<strong>')+8:]
-        #     else:
-        #         if processed_detail[processed_detail.find('<strong>')+8] == ' ':
-        #             processed_detail = processed_detail[:processed_detail.find('<strong>')]+' **'+processed_detail[processed_detail.find('<strong>')+9:]
-        #         else:
-        #             processed_detail = processed_detail[:processed_detail.find('<strong>')]+' **'+processed_detail[processed_detail.find('<strong>')+8:]
-        # while processed_detail.find('</strong') != -1:
-        #     if processed_detail[processed_detail.find('</strong>')+9] == ' ':
-        #         if processed_detail[processed_detail.find('</strong>')-1] == ' ':
-        #             processed_detail = processed_detail[:processed_detail.find('</strong>')-1]+'**'+processed_detail[processed_detail.find('</strong>')+9:]
-        #         else:
-        #             processed_detail = processed_detail[:processed_detail.find('</strong>')]+'**'+processed_detail[processed_detail.find('</strong>')+9:]
-        #     else:
-        #         if processed_detail[processed_detail.find('</strong>')-1] == ' ':
-        #             processed_detail = processed_detail[:processed_detail.find('</strong>')-1]+'** '+processed_detail[processed_detail.find('</strong>')+9:]
-        #         else:
-        #             processed_detail = processed_detail[:processed_detail.find('</strong>')]+'** '+processed_detail[processed_detail.find('</strong>')+9:]
-        # while processed_detail.find('<a ') != -1:
-            # print(len(processed_detail))
-            # print(processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+6)
-            # print(processed_detail[processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+6:].find('"')+processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+1)
-            # print(processed_detail[processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+6:processed_detail[processed_detail[processed_detail.find("<a "):].find('href="'):].find('"')+processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+1])
-            # processed_detail[processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+6:processed_detail[processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+6:].find('"')+processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+1]
-            # exit(0)
-        # while processed_detail.find('<a href="/') != -1:
-        #     hyperlink = processed_detail[processed_detail[processed_detail.find("""<a href=\""""):].find('href="')+processed_detail.find("<a ")+6:processed_detail[processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+6:].find('"')+processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+6]
-        #     print(processed_detail[processed_detail.find("<a "):])
-        #     if hyperlink.startswith('//'):
-        #         hyperlink = 'https:' + hyperlink
-        #     elif hyperlink.startswith('/'):
-        #         hyperlink = 'https://www.economist.com' + hyperlink
-        #     print(hyperlink)
-        #     processed_detail = processed_detail[:processed_detail.find('<a ')]+f'<a href="{hyperlink}">{processed_detail[processed_detail[processed_detail.find("<a "):].find(">")+processed_detail.find("<a ")+1:processed_detail[processed_detail.find("<a ")+1:].find("</a>")+processed_detail.find("<a ")+1]}</a>'+processed_detail[processed_detail.find('</a>')+4:]
-        #     break
-            # print(processed_detail)
-
-
-            # print(processed_detail)
-        # while processed_detail.find('<br/>') != -1:
-        #     processed_detail = processed_detail[:processed_detail.find('<br/>')]+'  \n'+processed_detail[processed_detail.find('<br/>')+5:]
         processed_detail = processed_detail.replace('<a href="//', '<a href="https://').replace('<a href="/', '<a href="https://economist.com/')
         processed_detail = processed_detail.replace('<a ', '<a class="mdui-text-color-theme" ')
         htmllines.append(f'<p>{processed_detail}</p>')
@@ -149,46 +94,6 @@ def web_process(**kwargs):
         htmllines.append(f'<h3>{titles[i]}</h3>')
         for j in range(len(details[i])):
             processed_detail = details[i][j]
-            # while processed_detail.find('<strong>') != -1:
-            #     if processed_detail[processed_detail.find('<strong>')-1] == ' ':
-            #         if processed_detail[processed_detail.find('<strong>')+8] == ' ':
-            #             processed_detail = processed_detail[:processed_detail.find('<strong>')]+'**'+processed_detail[processed_detail.find('<strong>')+9:]
-            #         else:
-            #             processed_detail = processed_detail[:processed_detail.find('<strong>')]+'**'+processed_detail[processed_detail.find('<strong>')+8:]
-            #     else:
-            #         if processed_detail[processed_detail.find('<strong>')-1] == ' ':
-            #             processed_detail = processed_detail[:processed_detail.find('<strong>')]+' **'+processed_detail[processed_detail.find('<strong>')+9:]
-            #         else:
-            #             processed_detail = processed_detail[:processed_detail.find('<strong>')]+' **'+processed_detail[processed_detail.find('<strong>')+8:]
-            # while processed_detail.find('</strong') != -1:
-            #     if processed_detail[processed_detail.find('</strong>')+9] == ' ':
-            #         if processed_detail[processed_detail.find('</strong>')-1] == ' ':
-            #             processed_detail = processed_detail[:processed_detail.find('</strong>')-1]+'**'+processed_detail[processed_detail.find('</strong>')+9:]
-            #         else:
-            #             processed_detail = processed_detail[:processed_detail.find('</strong>')]+'**'+processed_detail[processed_detail.find('</strong>')+9:]
-            #     else:
-            #         if processed_detail[processed_detail.find('</strong>')-1] == ' ':
-            #             processed_detail = processed_detail[:processed_detail.find('</strong>')-1]+'** '+processed_detail[processed_detail.find('</strong>')+9:]
-            #         else:
-            #             processed_detail = processed_detail[:processed_detail.find('</strong>')]+'** '+processed_detail[processed_detail.find('</strong>')+9:]
-            # while processed_detail.find('<a ') != -1:
-                # print(len(processed_detail))
-                # print(processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+6)
-                # print(processed_detail[processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+6:].find('"')+processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+1)
-                # print(processed_detail[processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+6:processed_detail[processed_detail[processed_detail.find("<a "):].find('href="'):].find('"')+processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+1])
-                # processed_detail[processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+6:processed_detail[processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+6:].find('"')+processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+1]
-                # exit(0)
-            # while processed_detail.find('href="/') != -1:
-            #     hyperlink = processed_detail[processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+6:processed_detail[processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+6:].find('"')+processed_detail[processed_detail.find("<a "):].find('href="')+processed_detail.find("<a ")+6]
-            #     if hyperlink.startswith('//'):
-            #         hyperlink = 'https:' + hyperlink
-            #     elif hyperlink.startswith('/'):
-            #         hyperlink = 'https://www.economist.com' + hyperlink
-            #     processed_detail = processed_detail[:processed_detail.find('<a ')]+f'<a href={hyperlink}>{processed_detail[processed_detail[processed_detail.find("<a "):].find(">")+processed_detail.find("<a ")+1:processed_detail[processed_detail.find("<a ")+1:].find("</a>")+processed_detail.find("<a ")+1]}</a>'+processed_detail[processed_detail.find('</a>')+4:]
-            #     break
-                # print(processed_detail)
-            # while processed_detail.find('<br/>') != -1:
-            #     processed_detail = processed_detail[:processed_detail.find('<br/>')]+'  \n'+processed_detail[processed_detail.find('<br/>')+5:]
             processed_detail = processed_detail.replace('<a href="//', '<a href="https://').replace('<a href="/', '<a href="https://economist.com/')
             processed_detail = processed_detail.replace('<a ', '<a class="mdui-text-color-theme" ')
             htmllines.append(f'<p>{processed_detail}</p>')
